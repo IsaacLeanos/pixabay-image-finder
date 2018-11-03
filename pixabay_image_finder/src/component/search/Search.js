5906a324314e6f05991ce772e6114880a56fc839
unknown <isaac.leanos11@gmail.com>
2018-11-02 21:33:48 -0700

    api search results

--- pixabay_image_finder/src/component/search/Search.js
+++ pixabay_image_finder/src/component/search/Search.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {Component}from 'react';
 import TextField from 'material-ui/TextField';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
+import axios from 'axios';
+import ImageResults from '../image-result/ImageResults';
 
 class Search extends React.Component {
 
@@ -10,16 +11,29 @@ class Search extends React.Component {
         searchText:'',
         amount:15,
         apiUrl:'https://pixabay.com/api',
-        apiKey:'',
+        apiKey:'10573179-5fe3c55bc0409172c4f17e306',
         images:[]
-    }
+    };
+
+    onTextChange=(e)=>{
+        this.setState({[e.target.name]:e.target.value},()=>{
+            axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo
+            &per_page=${this.state.amount}&safesearch=true`)
+            .then(res=>this.setState({images:res.data.hits}))
+            .catch(e=>console.log('error',e));
+        });
+    };
+
+    onAmountChange=(e,index,value)=>this.setState({amount:value});
 
   render() {
+    console.log(this.state.images)  
     return (
       <div>
 
         <TextField       
-            name='SearchText' value={this.state.searchText} 
+            name='searchText'
+            value={this.state.searchText} 
             onChange={this.onTextChange}
             floatingLabelText='Search for images..'
             fullWidth={true}
@@ -32,17 +46,15 @@ class Search extends React.Component {
             value={this.state.amount}
             onChange={this.onAmountChange}
         >
-            <MenuItem value="">
-            <em>None</em>
-            </MenuItem>
-            <MenuItem value={5}>Ten</MenuItem>
-            <MenuItem value={10}>Twenty</MenuItem>
-            <MenuItem value={15}>Thirty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
-            <MenuItem value={50}>Thirty</MenuItem>
+            <MenuItem value={5} primaryText={5}/>
+            <MenuItem value={10} primaryText={10}/>
+            <MenuItem value={15} primaryText={15}/>
+            <MenuItem value={30} primaryText={30}/>
+            <MenuItem value={50} primaryText={50}/>
         </SelectField>
 
           <br/>
+
       </div>
     )
   }
